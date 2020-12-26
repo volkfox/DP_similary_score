7354e8aa40b519d1d63dffa6e2d49c9b62b7784f
EC2 Default User <ec2-user@ip-172-30-0-20.us-west-2.compute.internal>
2020-12-26 01:58:05 +0000

    imperfect fix for http timeout issue

--- PythonHTTP/server.py
+++ PythonHTTP/server.py
@@ -80,6 +80,7 @@ def run(server_class=HTTPServer, handler_class=S, port=8000):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
+    httpd.timeout = 10
     logging.info('Starting httpd...\n')
     try:
         httpd.serve_forever()
